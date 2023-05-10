be0a52878d59f9c54f19f7d44b5b59caef8ba578
TaehyeonJeon <taehyun0540@gmail.com>
2023-05-10 14:21:32 +0900

    .

---
+++ README.md
@@ -0,0 +1,9 @@
+npm init
+
+npm install nodemon -D
+
+npm install @babel/core @babel/cli @babel/node @babel/preset -D
+
+npm install express
+
+npm install fug
---
+++ src/server.js
@@ -0,0 +1,24 @@
+import http from "http";
+import express from "express";
+import WebSocket from "ws";
+
+
+const app = express();
+
+app.set("view engine", "ejs")
+app.set("views", __dirname + "\\views")
+app.engine('html', require('ejs').renderFile);
+app.use("/public", express.static(__dirname + "/public"));
+app.get('/', (req, res) => res.render("index.html"))
+app.get('/capStoneMap.html', (req, res) => res.render("capStoneMap.html"))
+app.get('/capStoneCalender.html',(req,res) => res.render("capStoneCalender.html"))
+
+const handleListen = () => console.log('Hello');
+
+//http 서버
+const server = http.createServer(app);
+
+//ws 서버
+const wss = new WebSocket.Server({ server })
+
+server.listen(4000, handleListen)
