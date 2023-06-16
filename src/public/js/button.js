1d9861e35a19063680875b483d196bdf8ffdf427
TaehyeonJeon <taehyun0540@gmail.com>
2023-06-16 11:34:10 +0900

    .

--- src/public/js/button.js
+++ src/public/js/button.js
@@ -3,13 +3,13 @@ function backbutton() {
 }
 function icon1_click() {
 	//alert(s + ' 버튼을 누르셨습니다.');
-	location.href = 'http://127.0.0.1:4000/capStoneMap.html';
+	location.href = 'http://127.0.0.1:4000/capStoneMap';
 }
 function icon2_click() {
 	//alert(s + ' 버튼을 누르셨습니다.');
-	location.href = 'http://127.0.0.1:4000/Introduce.html';
+	location.href = 'http://127.0.0.1:4000/Introduce';
 }
 function icon3_click() {
 	//alert(s + ' 버튼을 누르셨습니다.');
-	location.href = 'http://127.0.0.1:4000/capStoneCalender.html';
+	location.href = 'http://127.0.0.1:4000/capStoneCalender';
 }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'http';
 import express from 'express';
-import WebSocket from 'ws';
+// import WebSocket from 'ws';
+import bodyParser from 'body-parser';
 
 const app = express();
 
@@ -8,28 +9,33 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '\\views');
 app.engine('html', require('ejs').renderFile);
 app.use('/public', express.static(__dirname + '/public'));
+app.use(
+	bodyParser.urlencoded({
+		extended: false,
+	})
+);
 app.get('/', (req, res) => res.render('index.html'));
-app.get('/capStoneMap.html', (req, res) => res.render('capStoneMap.html'));
-app.get('/introduce.html', (req, res) => res.render('introduce.html'));
-app.get('/capStoneCalender.html', (req, res) => res.render('capStoneCalender.html'));
+app.post('/', (req, res) => {
+	let search = req.body.search;
+	search = search.split(' ').join('');
+	if (search.indexOf('지도') > -1) {
+		res.redirect('/capStoneMap');
+	} else if (search.indexOf('일정') > -1) {
+		res.redirect('/capStoneCalender');
+	} else if (search.indexOf('학과소개') > -1) {
+		res.redirect('/introduce');
+	} else {
+		res.redirect('/');
+	}
+});
+app.get('/capStoneMap', (req, res) => res.render('capStoneMap.html'));
+app.get('/introduce', (req, res) => res.render('introduce.html'));
+app.get('/capStoneCalender', (req, res) => res.render('capStoneCalender.html'));
 app.use((req, res) => res.status(404).redirect('/'));
 
 const handleListen = () => console.log('Hello');
 
 //http 서버
 const server = http.createServer(app);
-
-//ws 서버
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (socket) => {
-	console.log('Connecting to Browser');
-	socket.on('close', () => {
-		console.log('Disconnecting to Browser');
-	});
-	socket.on('message', (message) => {
-		console.log(message.toString('utf-8'));
-	});
-});
 
 server.listen(4000, handleListen);
